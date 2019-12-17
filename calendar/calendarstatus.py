4f5b1404a0e20e004eea5de99cd040b73c9af3bb
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2019-12-17 01:39:23 +0000

    many update to test traversing widget class boundaries

--- calendar/calendarstatus.py
+++ calendar/calendarstatus.py
@@ -1,7 +1,8 @@
-
+from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
 from datetime import date
+from events import Event
 
 # Fetch current date when calendarstatus starts
 today = date.today()
@@ -23,12 +24,24 @@ class CalendarStatus(BoxLayout):
         self.cal_month.text = monthlabel
 
 
-
     def previous_month(self):
         print("You clicked the 'Previous Month' button")
 
 
         # TODO call functions to redraw Labels of the calendar widget for the previous month.
+        # Iterate through all widgets in events which is inside RelativeLayout calendar
+
+        for widget in self.walk(restrict=False,loopback=False):
+            if (type(widget)==Event):
+                #print(type(widget))
+                print("Widget ID's : {}".format(widget.ids))
+                print("Widget ID: {} ".format(widget.id))
+                print("Widget children: {}".format(widget.children))
+                widget.text = "hello"
+        # Place the days date into the correct widget based on the current month we are in. e.g. if we are in December 2019 then the 1st is on a Sunday which is widget number 7 in the grid layout.
+
+
+
         # calendarEvent['title'] = self.ids.title.text
         # calendarEvent['location'] = self.ids.location.text
         # calendarEvent['num_people'] = self.ids.num_people.text
@@ -42,3 +55,17 @@ class CalendarStatus(BoxLayout):
     def next_month(self):
         print("You clicked the 'next month' button")
         # TODO call functions to redraw Labesl of the calendar widget for the next month.
+
+        label_text = self.ids.month.text
+        print("CalendarStatus month text: {}".format(label_text))
+
+        label_text = self.parent.name
+        print("CalendarStatus name: {}".format(label_text))
+
+        label_text = self.parent.parent.ids._events.ids._e1.text
+        print("CalendarStatus ids: {}".format(label_text))
+
+        # using App.get_running_app()
+        #app_ref = App.get_running_app()
+        #print(app_ref.root, app_ref.root.ids.e1.text)
+
