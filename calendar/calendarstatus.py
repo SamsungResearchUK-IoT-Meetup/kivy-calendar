a5e35191efc8177525b79d6ac93935457393441d
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2019-12-18 00:45:03 +0000

    Adding functions to redraw gui for previous and next month

--- calendar/calendarstatus.py
+++ calendar/calendarstatus.py
@@ -1,11 +1,16 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
-from datetime import date
+import datetime
+from datetime import date, timedelta
 from events import Event
+from calendar import monthrange
 
 # Fetch current date when calendarstatus starts
 today = date.today()
+set_date = date.today()
+# Create a dictionary to map a day to a column. Used to calculate when to print 'date' onto an event.
+col_day = {'Monday':1,'Tuesday':2, 'Wednesday':3, 'Thursday':4, 'Friday':5, 'Saturday':6, 'Sunday': 7}
 
 class CalendarStatus(BoxLayout):
 
@@ -17,55 +22,117 @@ class CalendarStatus(BoxLayout):
         print("Callback has been run")
         current_month = today.strftime("%B")
         current_year = today.strftime("%Y")
+        current_day = today.strftime("%A")
 
         monthlabel = "[size=24px][color=#37abc8ff] {} [/color]".format(current_month)
         yearlabel = "[size=18px][color=#37abc8ff][i] {} [/i][/color]".format(current_year)
         self.cal_year.text = yearlabel
         self.cal_month.text = monthlabel
 
+        # Calculate First day of current month
+        monthrange_tuple = monthrange(today.year, today.month)
+        first_day_of_month_num = monthrange_tuple[0]
+        num_of_days_in_month = monthrange_tuple[1]
+
+        # Loop over all monthly events. There are 42. But we only place a date onto an event beginning on the first day of the Month and count upwards.
+        for idx, event in enumerate(self.parent.parent.ids._events.ids):
+            print("\nindex is: {}  Event is: {}".format(idx, event))
+            if (idx>=first_day_of_month_num) & (idx <= (first_day_of_month_num + num_of_days_in_month-1)):
+                new_date_text = idx - first_day_of_month_num + 1
+                print( "**** Start populating events **** Date: {}".format(new_date_text))
+                print(" Current event text is: {}.".format(self.parent.parent.ids._events.ids[event].text))
+
+                self.parent.parent.ids._events.ids[event].text = "[size=10px][color=#37abc8ff][i]{}[/i][/color]".format(new_date_text)
+            else:
+                self.parent.parent.ids._events.ids[event].text = "[size=10px][color=#37abc8ff][i]{}[/i][/color]".format(" ")
+
 
     def previous_month(self):
         print("You clicked the 'Previous Month' button")
 
+        # Move the variable 'selected_date' into the previous month
+        global set_date
+        selected_date = set_date.replace(day=1)
+        selected_date = selected_date - datetime.timedelta(days=1)
 
-        # TODO call functions to redraw Labels of the calendar widget for the previous month.
-        # Iterate through all widgets in events which is inside RelativeLayout calendar
+        # Calculate First day of previous month
+        monthrange_tuple = monthrange(selected_date.year, selected_date.month)
+        first_day_of_month_num = monthrange_tuple[0]
+        num_of_days_in_month = monthrange_tuple[1]
 
-        for widget in self.walk(restrict=False,loopback=False):
-            if (type(widget)==Event):
-                #print(type(widget))
-                print("Widget ID's : {}".format(widget.ids))
-                print("Widget ID: {} ".format(widget.id))
-                print("Widget children: {}".format(widget.children))
-                widget.text = "hello"
-        # Place the days date into the correct widget based on the current month we are in. e.g. if we are in December 2019 then the 1st is on a Sunday which is widget number 7 in the grid layout.
+        # This sets the 'Month' and 'Year' Labels at the top of the calendar status GUI element to the previous month
+        selected_month = selected_date.strftime("%B")
+        selected_year = selected_date.strftime("%Y")
+        monthlabel = "[size=24px][color=#37abc8ff] {} [/color]".format(selected_month)
+        yearlabel = "[size=18px][color=#37abc8ff][i] {} [/i][/color]".format(selected_year)
+        self.cal_year.text = yearlabel
+        self.cal_month.text = monthlabel
+
+        # TODO make this iterator a function or method of the class. It's exactly the same for the set_date_callback, previous_month and next_month
+        # Loop over all monthly events. There are 42. But we only place a date onto an event beginning on the first day of the Month and count upwards.
+        for idx, event in enumerate(self.parent.parent.ids._events.ids):
+            print("\nindex is: {}  Event is: {}".format(idx, event))
+            if (idx>=first_day_of_month_num) & (idx <= (first_day_of_month_num + num_of_days_in_month-1)):
+                new_date_text = idx - first_day_of_month_num + 1
+                print( "**** Start populating events **** Date: {}".format(new_date_text))
+                print(" Current event text is: {}.".format(self.parent.parent.ids._events.ids[event].text))
+
+                self.parent.parent.ids._events.ids[event].text = "[size=10px][color=#37abc8ff][i]{}[/i][/color]".format(new_date_text)
+            else:
+                self.parent.parent.ids._events.ids[event].text = "[size=10px][color=#37abc8ff][i]{}[/i][/color]".format(" ")
+
+        # Make sure you set the new set date to the previous month
+        set_date = selected_date
 
 
+        # for widget in self.walk(restrict=False,loopback=False):
+        #     if (type(widget)==Event):
+        #         #print(type(widget))
+        #         print("Widget ID's : {}".format(widget.ids))
+        #         print("Widget ID: {} ".format(widget.id))
+        #         print("Widget children: {}".format(widget.children))
+        #         widget.text = "hello"
 
-        # calendarEvent['title'] = self.ids.title.text
-        # calendarEvent['location'] = self.ids.location.text
-        # calendarEvent['num_people'] = self.ids.num_people.text
-        # calendarEvent['optional_people'] = self.ids.optional_people.text
-        # calendarEvent['meeting_length'] = self.ids.slider_meeting_length.value
-        # calendarEvent['earliest_date'] = self.ids.earliest_date
-        # calendarEvent['latest_data'] = self.ids.latest_date
-        # calendarEvent['shedule_deadline'] = self.ids.schedule_deadline
-        # calendarEvent['priority'] = self.ids.slider_priority.value
 
     def next_month(self):
         print("You clicked the 'next month' button")
-        # TODO call functions to redraw Labesl of the calendar widget for the next month.
 
-        label_text = self.ids.month.text
-        print("CalendarStatus month text: {}".format(label_text))
+        # Move the variable 'selected_date' into the next month
+        global set_date
+        selected_date = set_date.replace(day=1)
+        selected_date = selected_date + datetime.timedelta(days=32)
+        selected_date = selected_date.replace(day=1)
 
-        label_text = self.parent.name
-        print("CalendarStatus name: {}".format(label_text))
+        # Calculate First day of previous month
+        monthrange_tuple = monthrange(selected_date.year, selected_date.month)
+        first_day_of_month_num = monthrange_tuple[0]
+        num_of_days_in_month = monthrange_tuple[1]
 
-        label_text = self.parent.parent.ids._events.ids._e1.text
-        print("CalendarStatus ids: {}".format(label_text))
+        # This sets the 'Month' and 'Year' Labels at the top of the calendar status GUI element to the previous month
+        selected_month = selected_date.strftime("%B")
+        selected_year = selected_date.strftime("%Y")
+        monthlabel = "[size=24px][color=#37abc8ff] {} [/color]".format(selected_month)
+        yearlabel = "[size=18px][color=#37abc8ff][i] {} [/i][/color]".format(selected_year)
+        self.cal_year.text = yearlabel
+        self.cal_month.text = monthlabel
 
-        # using App.get_running_app()
-        #app_ref = App.get_running_app()
+        # TODO make this iterator a function or method of the class. It's exactly the same for the set_date_callback, previous_month and next_month
+        # Loop over all monthly events. There are 42. But we only place a date onto an event beginning on the first day of the Month and count upwards.
+        for idx, event in enumerate(self.parent.parent.ids._events.ids):
+            print("\nindex is: {}  Event is: {}".format(idx, event))
+            if (idx>=first_day_of_month_num) & (idx <= (first_day_of_month_num + num_of_days_in_month-1)):
+                new_date_text = idx - first_day_of_month_num + 1
+                print( "**** Start populating events **** Date: {}".format(new_date_text))
+                print(" Current event text is: {}.".format(self.parent.parent.ids._events.ids[event].text))
+
+                self.parent.parent.ids._events.ids[event].text = "[size=10px][color=#37abc8ff][i]{}[/i][/color]".format(new_date_text)
+            else:
+                self.parent.parent.ids._events.ids[event].text = "[size=10px][color=#37abc8ff][i]{}[/i][/color]".format(" ")
+
+        # Make sure you set the new set date to the previous month
+        set_date = selected_date
+
+
+
         #print(app_ref.root, app_ref.root.ids.e1.text)
 
