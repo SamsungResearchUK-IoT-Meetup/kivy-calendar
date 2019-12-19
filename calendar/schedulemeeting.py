a352662b79d983ab0a8817e4f7c2b992dfb03d70
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2019-12-19 11:09:29 +0000

    Adding email verification for 'required people' field

--- calendar/schedulemeeting.py
+++ calendar/schedulemeeting.py
@@ -20,14 +20,30 @@ SOFTWARE.
 """
 
 from kivy.uix.stacklayout import StackLayout
+from email.utils import parseaddr
 
 class ScheduleMeeting(StackLayout):
 
+    def check_email(self, dirty_string):
+        # Validate Num People. This is a list of emails separated via ';'. So we need split the text string into a list, remove white space and then validate
+        clean_list = []
+        dirty_list = dirty_string.split(';')
+
+        for email in dirty_list:
+            email = email.strip()
+            clean_email = parseaddr(email)
+            if (clean_email[1]==''):
+                return (False, dirty_list)
+            clean_list.append(clean_email)
+        return (True, clean_list)
+
     def start(self):
+
+        validated = True
         calendarEvent = {
             "title": None,
             "location": None,
-            "num_people": None,
+            "required_people": None,
             "optional_people": None,    # TODO make this a list of email addresses that are checked and formatted
             "meeting_length": 0,
             "earliest_date": 0,         # TODO Make this a time date object
@@ -39,14 +55,28 @@ class ScheduleMeeting(StackLayout):
         print("You clicked the 'Start Schedule' button")
         calendarEvent['title'] = self.ids.title.text
         calendarEvent['location'] = self.ids.location.text
-        calendarEvent['num_people'] = self.ids.num_people.text
+        #calendarEvent['required_people'] = self.ids.required_people.text
         calendarEvent['optional_people'] = self.ids.optional_people.text
         calendarEvent['meeting_length'] = self.ids.slider_meeting_length.value
         calendarEvent['earliest_date'] = self.ids.earliest_date
         calendarEvent['latest_data'] = self.ids.latest_date
         calendarEvent['shedule_deadline'] = self.ids.schedule_deadline
         calendarEvent['priority'] = self.ids.slider_priority.value
-        print("The calendar event for the title is now set to: {}".format(calendarEvent))
+
+        # Validate emails for 'required people' field
+        cleaned_email = self.check_email(self.ids.required_people.text)
+        if cleaned_email[0]:
+            calendarEvent['required_people'] = cleaned_email[1]     # Place the cleaned list of emails into the dictionary
+        else:
+            self.ids.required_people.text = 'Illegal value, try: my.name@email.com'
+            self.ids.required_people_label.text = '[color=#ff0000][b]Required people[/b][/color]'
+            validated = False
+
+        if validated:
+            # Pass values to the agent to be calculated
+            self.ids.required_people_label.text = '[b]Required people[/b]'
+            print("Calculating best time for calendar invite...")
+            #
 
         # TODO Pass this dictionary to the 'Calculate' event function
         # On response:  1) Capture output iCalendar events and place in a calendar event object.
